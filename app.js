fd32ce4bcf153dbc5fbd227643f183592da152ed
Vukosi <vukosimohlabini16@gmail.com>
2024-02-10 14:08:55 +0200

    Complete buttons and screen functionality
    All buttons display their text on the screen.

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
-const buttons = document.querySelectorAll("button");
+const numbers = document.querySelectorAll(".numbers");
+const screen = document.querySelector(".display");
+const operators = document.querySelectorAll(".operations");
+const clearbutton = document.querySelector(".clear");
 
-buttons.onmouseover = () => buttons.style.backgroundColor = "white";
+numbers.forEach(number => {
+  number.addEventListener("mouseover", () => {
+    number.style.backgroundColor = "white";
+  });
+
+  number.addEventListener("mouseout", () => {
+    number.style.backgroundColor = "grey";
+  });
+
+  number.addEventListener("click", () => {
+    screen.textContent += number.textContent;
+  });
+});
+
+operators.forEach(operators => {
+    operators.addEventListener("mouseover", () => {
+        operators.style.backgroundColor = "white";
+  });
+
+  operators.addEventListener("mouseout", () => {
+    operators.style.backgroundColor = "grey";
+  });
+
+  operators.addEventListener("click", () => {
+    screen.textContent += operators.textContent;
+  });
+}); 
+
+clearbutton.addEventListener("click", () => {screen.textContent = ""});
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", () => {console.log(screen.textContent)});
+
+
+
+
+
